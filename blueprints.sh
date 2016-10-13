8192371d7d982bcdc055ca96787c36e929c1c649
Michael Rosata <mrosata@users.noreply.github.com>
2016-10-13 11:46:02 -0400

    Update blueprints.sh

--- blueprints.sh
+++ blueprints.sh
@@ -14,68 +14,19 @@
 EDITOR=${EDITOR:-vi}
 PAGER=${PAGER:-less}
 
-program_description="
-  This script is intended to make your boilerplate code not only easier to
-  manage, but also simplier to setup, configure and share. You can quickly
-  add a new empty template to the system or generate a template from a file
-  that is already on your computer. Eventually you will be able to include
-  config files within projects that define boilerplate components/templates 
-  for that project.
-"
+function program_instruction () {
+  # Echo out long description
+  . "$SCRIPTPATH/bin/.program-instruction"
+}
 
-program_instruction="
-  new [COMMAND] [-nhvfBe] [-tb DIRECTORY] BLUEPRINT DEST
-
-  \"Required Information\"
-
-    BLUEPRINT
-      - Name of blueprint template to use. The \"BLUEPRINT\" name will be
-        used to determine how to create, store, reproduce, edit and remove
-        a specific blueprint.
-
-    DEST
-      - Name to be used in the templates recreated by the script inside you
-        project or working folder. In the case that multiple files are created
-        by a blueprint, the configuration file should handle ensuring that
-        no collisions occur
-
-
-  \"Variables\"
-
-    -t TARGET_DIR
-        This variable is the target root directory. If you are in directory
-        \"/projects\" then by default TARGET_DIR=\"/projects\", but that may not
-        be where generated boilerplate files go. For instance, a configuration
-        file might set a base_dir=\"app\" in which case that boilerplate file
-        would be created in \"/projects/app\".
-
-    -b BACKUP_DIR
-        In most cases generated boilerplate shouldn't overwrite other files, but 
-        in the case where a file is overwritten a backup is created (unless user
-        explicity sets -B). Generally you should not care to touch this variable.
-        If you change this variable for a run, you also need to set it if and 
-        when you want to restore the old file.
-
-
-  \"Flags\"
-    
-    -h   \"Help\", show this screen.
-    -n   \"No change\", meaning no files will be created/altered/removed this run.
-    -v   \"Verbose\", the verbosity level can be set by using multiple \"v\" flags
-    -f   \"\",
-    -B   \"No Backup\", if a file is overwritten do not create a backup. Generally
-         templates shouldn't overwrite files.
-    -e   \"Edit\", open editor upon any change. Uses the global EDITOR|-vi
-
-"
 
 ################################################################################
 ####
 ####  "Environment Variables"
 ####  
 ####  -configurable by environment (configs w/defaults if not already set)
-####    NEW_SCRIPTPATH    ->  ~/bin/new
-####    NEW_TEMPLATE_DIR  ->  ~/bin/custom/templates  -->  "template storage"
+####    NEW_TEMPLATE_DIR  ->  $SCRIPTPATH/templates  -->  "template storage"
+####    NEW_BACKUP_DIR    ->  $SCRIPTPATH/.backups   -->  "backups storage"
 ####
 
 # push the directory of this script onto dir stack to save the directory
@@ -83,11 +34,18 @@ pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd`
 popd > /dev/null
 
-NEW_SCRIPTPATH="${NEW_SCRIPTPATH:-$SCRIPTPATH}" # base of template & backups
-NEW_IMPORTS_DIR="$NEW_SCRIPTPATH/bin"
-NEW_TEMPLATES_DIR="${NEW_TEMPLATES_DIR:-$NEW_SCRIPTPATH/templates}"
-NEW_BACKUP_DIR="${NEW_BACKUP_DIR:-$NEW_SCRIPTPATH/.backups}"
+NEW_IMPORTS_DIR="$SCRIPTPATH/bin"
+NEW_TEMPLATES_DIR="${NEW_TEMPLATES_DIR:-$SCRIPTPATH/templates}"
+NEW_BACKUP_DIR="${NEW_BACKUP_DIR:-$SCRIPTPATH/.backups}"
 
+function run_create_new_script() 
+  { . "$NEW_IMPORTS_DIR/create-new.sh"; }
+
+function run_destroy_script()
+  { . "$NEW_IMPORTS_DIR/destroy-new.sh"; }
+
+function run_generate_new_script ()
+  { . "$NEW_IMPORTS_DIR/generate-new.sh"; }
 
 ################################################################################
 ####
@@ -98,23 +56,32 @@ NEW_BACKUP_DIR="${NEW_BACKUP_DIR:-$NEW_SCRIPTPATH/.backups}"
 ####     new templatename destpath
 ####
 
-function print_program_description() {
+function print_small_description() {
   echo "  \"new\" AKA \"blueprints.sh\"
     Flexible blueprinting. Pass -h for help"
 }
 
 function print_program_help() {
-  echo -ne program_description
-  echo -ne program_instruction
+  program_instruction | $PAGER
+}
+
+# Shows all the templates in the new templates directory 
+function display_templates_list () {
+  if [ -d "$NEW_TEMPLATES_DIR" ];then
+    echo "Found $(ls -1 $NEW_TEMPLATES_DIR | wc -l) templates:"
+    list_subdirectories_in $NEW_TEMPLATES_DIR '    '
+  fi
 }
 
 function show_folder_locations() {
   echo "
-  New (blueprints) folder locations:
+  New (blueprints) folder locations (mainly for debug purposes):
 
+  - New bin directory (where program is):
+      $SCRIPTPATH
   - Template directory: 
       $NEW_TEMPLATES_DIR
-  - Write directory: 
+  - Base Write directory (your dir): 
       $TARGET_DIR"
 }
 
@@ -137,25 +104,36 @@ fi
 ####  "Variables and Exit Codes"
 ####
 ####  -exit codes
-####     EXIT_OK   ->  0   -->  "Successful exit"
-####     E_TMPERR  ->  11  -->  "Template not found"
-####     E_DSTERR  ->  12  -->  "Destination files directory not exist"
-####	   E_BKUPERR ->  13  -->  "Base backup directory not exist"
-####	   E_OPTERR  ->  65  -->  "Invalid Argument"
+####     EXIT_OK     ->  0   -->  "Successful exit"
+####     EXIT_USER   ->  1   -->  "User prompted exit (maybe a y/n prompt)"
+####     E_DSTERR    ->  11  -->  "Destination files directory not exist"
+####     E_BKUPERR   ->  12  -->  "Base backup directory not exist"
+####     E_DIRERR    ->  12  -->  "Directory error"
+####     E_TMPERR    ->  21  -->  "Template not found"
+####     E_NAMERR    ->  22  -->  "Name not allowed"
+####     E_EMTYOPT   ->  41  -->  "Zero Arguments"
+####	   E_OPTERR    ->  42  -->  "Invalid Argument"
 ####
 
 # Base Variables
-VERBOSE_MSG=""
+VERBOSE_MSG=
 
 # Exit Codes
 EXIT_OK=0
 EXIT_USER=1
-E_OPTERR=65
-E_TMPERR=11
-E_DSTERR=12
-E_BKUPERR=13
-E_DIRERR=14
+# Directory errors
+E_DSTERR=11
+E_BKUPERR=12
+E_DIRERR=13
+# run specific errors
+E_TMPERR=21
+E_NAMERR=22
+# Option errors
+E_EMTYOPT=41
+E_OPTERR=42
 
+# Exit the program and also display an error message if
+# the argument $1 is an error code (greater than 0) 
 function exit_with_code() {
 
   # If verbose was set then show messages
@@ -166,20 +144,26 @@ function exit_with_code() {
   # Echo out error message
   if [ "$1" -gt "0" ];then
     case $1 in
+      $E_EMTYOPT)
+        echo "  Warning: No options found."
+        ;;
   	  $E_OPTERR)
-	      echo "  Error: invalid option" >&2
+	      echo "  Error: invalid option." >&2
 	      ;;
-    $E_TMPERR)
-        echo "  Error: template error" >&2
+      $E_TMPERR)
+        echo "  Error: template error." >&2
         ;;
-    $E_DIRERR)
-        echo "  Error: directory error" >&2
+      $E_DIRERR)
+        echo "  Error: directory error." >&2
         ;;
-    $E_DSTERR)
-        echo "  Error: destination error" >&2
+      $E_DSTERR)
+        echo "  Error: destination error." >&2
         ;;
-    $E_BKUPERR)
-        echo "  Error: backup directory error" >&2
+      $E_BKUPERR)
+        echo "  Error: backup directory error." >&2
+        ;;
+      $E_NAMERR)
+        echo "  Error: Name not allowed." >&2
         ;;
     esac
   fi
@@ -196,7 +180,7 @@ function exit_with_code() {
 ####      -v            - "verbose - display what changes will be made."
 ####	    -n            - "No Change - don't actually make changes, trial run."
 ####      -f            - "Folders - show the folder locations"
-####      -b            - "Backup folder" - default to $NEW_SCRIPTPATH/backups
+####      -b            - "Backup folder" - default to $SCRIPTPATH/backups
 ####      -B            - "No backups" - default is to create backups
 ####      -e            - "Edit on create" - use flag to edit new blueprint
 ####
@@ -211,16 +195,31 @@ NO_BACKUPS=0
 NO_CHANGE=0
 VERBOSE=0
 
-# If there are no arguments passed in, show description
-if [ "$#" -lt 2 ]
-then
-  if [ "$1" = "-h" ];then
+
+# Check if user passed -h|help -l|list or info
+function check_arguments_help_info_list () {
+  if [ "$1" = "-h" ] || [ "$1" = "help" ];then
     print_program_help
-    exit_with_code $E_OPTERR
-  else
-    print_program_description
-    exit_with_code $E_OPTERR
+    exit_with_code $EXIT_OK
+  
+  elif [ "$1" = "-l" ] || [ "$1" = "list" ];then
+    display_templates_list
+    exit_with_code $EXIT_OK
+  
+  elif [ "$1" = "info" ];then
+    print_small_description
+    exit_with_code $EXIT_OK
   fi
+}
+
+
+check_arguments_help_info_list "$1"
+
+# If there are no arguments passed in, show description
+if [ "$#" -eq "0" ]
+then
+  print_small_description
+  exit_with_code $E_EMTYOPT
 fi
 
 
@@ -229,7 +228,7 @@ if [ "$1" = "new" ];then
   shift
 fi
 
-if [ "$1" = "destroy" ] || [ "$1" = "rm" ] || [ "$1" = "remove" ];then
+if [ "$1" = "destroy" ];then
   DESTROY_TEMPLATE=1
   shift
 fi
@@ -277,7 +276,7 @@ do
     # Invalid Arguments. Display if verbose
     \?)
       UNKNOWN_ARG=1
-      add_verbose_msg "invalid option: -$opt $OPTARG"
+      add_verbose_msg "unknown argument: -$opt $OPTARG"
       ;;
   esac
 done
@@ -285,23 +284,30 @@ done
 # Check for 2 positional args: template and destination filename
 blueprint=${@:$OPTIND:1}
 destname=${@:$OPTIND+1:1}
-if [ "$CREATE_NEW" -eq "1" ];then
-  . "$NEW_IMPORTS_DIR/create-new.sh"
-  exit_with_code $EXIT_OK
+
+if [ "$CREATE_NEW" -eq "1" ]
+  then
+    run_create_new_script
+    exit_with_code $EXIT_OK
+
+elif [ "$DESTROY_TEMPLATE" -eq "1" ]
+  then
+    run_destroy_script
+    exit_with_code $EXIT_OK
 fi
 
-if [ "$DESTROY_TEMPLATE" -eq "1" ];then
-  . "$NEW_IMPORTS_DIR/destroy-new.sh"
-  exit_with_code $EXIT_OK
-fi
-
-if [ -z "$blueprint" ] || [ -z "$destname" ];then
-  echo "Must pass blueprint name and destname as last 2 positional args"
-fi
 
 # Exit from unknown only after all args passed.
 if [ "$UNKNOWN_ARG" -gt "0" ];then
   exit_with_code $E_OPTERR
+fi
+
+
+# NOTE: This test should only be reached if there isn't some pre-argument
+#       commands run such as `new`, `destroy` or `edit`. This area should
+#       probably be re-thought, as it isn't not easy to understand.
+if [ -z "$blueprint" ] || [ -z "$destname" ];then
+  echo -e "Must pass blueprint name and destname as last 2 positional args"
 fi
 
 
@@ -317,25 +323,28 @@ if [ ! -d "$blueprint_folder" ] || [ ! -f "$blueprint_config" ];then
   exit_with_code $E_TMPERR
 fi
 
+
 # Generate the boilerplate code
-. "$NEW_IMPORTS_DIR/generate-new.sh"
+run_generate_new_script
 
 # Exit Successfully 
 exit_with_code $EXIT_OK
 
+
 ################################################################################
 ####
 ####   "Will-Change"
-####     >- $NEW_TEMPLATES_DIR will be a "default" value which shall easily   
+####    >-  $NEW_TEMPLATES_DIR will be a "default" value which shall easily   
 ####        be overwritten by any number of configuration files located in  
 ####        the users $HOME_DIR or project folders.
-####     >- Somehow variables will be available for use inside templates. This
-####        feature requires fore-thought since blueprints should work for any
-####		file type it must be careful about how it chooses to differenciate
-####        between variables intented to be templated in and similar syntax 
-####        that is just meant to be part of the template. (perhaps the variables)
+####
+####    >-  Somehow variables will be available for use inside templates. This
+####        feature requires some thought since "new"/blueprints must work for
+####		    all file types, so it must be careful about how it chooses to identify
+####        variables intented to be templated and boilerplate with similar syntax 
+####        that is just meant to be part of the template. (perhaps the variables
 ####        will be explict mappings between search and replace values)
 ####        
-####    >- There will be a restore. The backup is already done.        
+####    >-  There will be a restore. The backup is already done.        
 ####        
 ####
