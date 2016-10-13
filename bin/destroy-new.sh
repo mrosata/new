e1f938a5f7847fec1771624c1967eda6e3f984f0
Michael Rosata <mrosata@users.noreply.github.com>
2016-10-13 11:47:51 -0400

    Update destroy-new.sh

--- bin/destroy-new.sh
+++ bin/destroy-new.sh
@@ -13,32 +13,33 @@
 
 EDITOR=${EDITOR:-vi}
 PAGER=${PAGER:-less}
-new_create_folder="$NEW_TEMPLATES_DIR/$blueprint"
+new_destroy_folder="$NEW_TEMPLATES_DIR/$blueprint"
 
 if [ -z "$blueprint" ];then
-  exit_with_code 65
+  exit_with_code $E_MISS
 fi
 
 
-if [ -d $new_create_folder ];then
+if [ -d $new_destroy_folder ];then
   # PROMPT THE USER IF THEY WANT TO OVERWRITE/EDIT/EXIT
-  echo  "About to completely remove template $blueprint\!"
-  echo -n "   - Are you sure?  -press y/n then [ENTER]"
+  echo  -e "  About to completely remove template $blueprint!"
+  echo -n "     - Are you sure?  -press y/n then [ENTER]"
   read ans
   if [ $? != 0 ] || [[ ! $ans =~ ^y(es)?$ ]];then
     add_verbose_msg "fewww, that was close, exiting instead of destroying folder"
     exit_with_code $EXIT_USER
   fi
   # Remove
-  rm -r $new_create_folder
+  rm -r $new_destroy_folder
   # Make sure the directory no longer exists
-  if [ -d $new_create_folder ];then
-    add_verbose_msg "Unable to remove folder, try 'rm -rf $new_create_folder'"
+  if [ -d $new_destroy_folder ];then
+    add_verbose_msg "Unable to remove folder, try 'rm -rf $new_destroy_folder'"
   else
     add_verbose_msg "Removed template $blueprint"
   fi
 
 else
   # The directory to destroy doesn't exist
-  add_verbose_msg "Template $blueprint doesn't exist so it can't be destroyed"
+  echo "  Unable to destroy blueprint \"$blueprint\" because it can't be found!"
+  echo "    - hint: running \`new list\` should display local templates."
 fi
